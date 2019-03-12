c3300a3cba6cdf1d7ee5a5c9cdd5ef4be2de424b
felipemendes <f.ribeiromendes@gmail.com>
2019-03-12 20:32:12 -0300

    Initial commit

---
+++ Example/iHash/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iHash
+//
+//  Created by felipemendes on 03/12/2019.
+//  Copyright (c) 2019 felipemendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
