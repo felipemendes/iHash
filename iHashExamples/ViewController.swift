10d145ffc2b01f0fedcf257daa3b9d7419a6d940
felipemendes <f.ribeiromendes@gmail.com>
2019-03-14 20:00:59 -0300

    Add iHashExamples target

---
+++ iHashExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHashExamples
+//
+//  Created by Felipe Mendes on 14/03/19.
+//  Copyright © 2019 Felipe Mendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
