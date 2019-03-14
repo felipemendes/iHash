3db2c941f2a3312ea3cf56501435dd0ed7d2a020
felipemendes <f.ribeiromendes@gmail.com>
2019-03-14 20:34:50 -0300

    Add test case

--- iHashTests/iHashTests.swift
+++ iHashTests/iHashTests.swift
@@ -11,8 +11,16 @@ import XCTest
 
 class iHashTests: XCTestCase {
 
+    var ihash: iHash!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        ihash = iHash()
+    }
+    
+    func testMD5() {
+        let test: String = "testing"
+        XCTAssertEqual(ihash.md5(test), "ae2b1fca515949e5d54fb22b8ed95575")
     }
 
     override func tearDown() {
